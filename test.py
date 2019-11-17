55371da4e8b8d696e396a609325c598cf92563e1
Kevin Basuki <kevinb9g.17@gmail.com>
2019-11-17 21:22:37 +0700

    Add Dbscan test

--- test.py
+++ test.py
@@ -3,12 +3,14 @@ import numpy as np
 from sklearn import datasets
 from sklearn.cluster import KMeans
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import DBSCAN
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 # Algorithms
 from hierarchical import AgglomerativeClustering as ac
 from kmeans import KMeansAlg
+from dbscan import DbscanClustering as dc
 
 # Load Iris dataset
 iris = datasets.load_iris()
@@ -27,18 +29,19 @@ print("y Train shape : " + str(y_train.shape))
 print("y Test shape  : " + str(y_test.shape))
 print("")
 
-print("== KMeans with self-created algorithm ==")
-kmeans_self = KMeansAlg(n_clusters=3)
-kmeans_self.fit(X_train)
-y_pred_kmeans_self = kmeans_self.predict(X_test)
-print("Accuracy Score: " + str(accuracy_score(y_test, y_pred_kmeans_self)))
+print("== DBSCAN with self-created algorithm ==")
+dbs = dc(epsilon=0.7, min_pts=9, distance_metric='euclidean')
+dbs.count_distance(X_test, X_train)
+dbs.fit(X_train)
+y_pred_dbs_self = dbs.predict(X_test)
+print("Accuracy Score: " + str(accuracy_score(y_test, y_pred_dbs_self)))
 print("")
 
-print("== KMeans with scikit-learn library ==")
-kmeans_sklearn = KMeans(n_clusters=3)
-kmeans_sklearn.fit(X_train)
-y_pred_kmeans_sklearn = kmeans_sklearn.predict(X_test)
-print("Accuracy Score: " + str(accuracy_score(y_test, y_pred_kmeans_sklearn)))
+print("== DBSCAN with scikit-learn library ==")
+dbs_sklearn = DBSCAN(eps=0.7, min_samples=9, metric='euclidean')
+dbs_sklearn.fit(X_train)
+y_pred_dbs_sklearn = dbs_sklearn.labels_
+print("Accuracy Score: " + str(accuracy_score(y_train, y_pred_dbs_sklearn)))
 print("")
 
 print("== Agglomerative with self-created algorithm ==")
@@ -53,4 +56,18 @@ agglo_sklearn = AgglomerativeClustering(n_clusters=3, linkage='single', affinity
 agglo_sklearn.fit(X_train)
 y_pred_agglo_sklearn = agglo_sklearn.labels_
 print("Accuracy Score: " + str(accuracy_score(y_train, y_pred_agglo_sklearn)))
+print("")
+
+print("== KMeans with self-created algorithm ==")
+kmeans_self = KMeansAlg(n_clusters=3)
+kmeans_self.fit(X_train)
+y_pred_kmeans_self = kmeans_self.predict(X_test)
+print("Accuracy Score: " + str(accuracy_score(y_test, y_pred_kmeans_self)))
+print("")
+
+print("== KMeans with scikit-learn library ==")
+kmeans_sklearn = KMeans(n_clusters=3)
+kmeans_sklearn.fit(X_train)
+y_pred_kmeans_sklearn = kmeans_sklearn.predict(X_test)
+print("Accuracy Score: " + str(accuracy_score(y_test, y_pred_kmeans_sklearn)))
 print("")
